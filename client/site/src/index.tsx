9d1d796ff372941d5dd7800c9cdc5733feeb1073
Victor Ionescu <v.ionescu@student.tudelft.nl>
2018-04-27 16:53:21 +0200

    add provider for redux

--- client/site/src/index.tsx
+++ client/site/src/index.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import MainContainer from "./MainContainer";
+import mainReducer from "./redux/reducers/";
 
+const store = createStore(mainReducer);
 
-render(<MainContainer />, document.getElementById("container"));
+render(
+	<Provider store={store}>
+		<MainContainer />
+	</Provider>,
+	document.getElementById("container"));
