364567e16f4aac81a03e2e87b7503785c647ce73
Victor Ionescu <victorpionescu@gmail.com>
2018-05-24 03:00:06 +0200

    set new bio as test bio; fix fetchBio on site

--- client/site/src/Pages/Bio/index.tsx
+++ client/site/src/Pages/Bio/index.tsx
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import axios, {AxiosResponse} from "axios";
 import {setBio} from "../../redux/actions/bio";
 import * as showdown from "showdown";
+import MarkdownPreview from "../../../../menu/src/Editor/MarkdownPreview";
 const styles = require("./styles.less");
 
 const bioURL = "/bioData";
@@ -29,7 +30,7 @@ class Bio extends React.Component<BioProps, {}> {
 
 	fetchBio() {
 		axios.get(bioURL).then((response: AxiosResponse) => {
-			const bio = response.data.bio;
+			const bio = response.data.data;
 			this.props.setBio(bio);
 			this.setState({
 				text: bio
@@ -39,50 +40,8 @@ class Bio extends React.Component<BioProps, {}> {
 
 	render() {
 		return (
-			//<div className={styles.bio} dangerouslySetInnerHTML={{__html: this.converter.makeHtml(this.props.bio)}}>
 			<div  className={styles.bio}>
-				<p>
-					<strong>
-				Wie ben je als je uit je schulp kruipt?
-					</strong>
-					<br/>
-					De driekoppige band Out Of Skin wil het hele spectrum van het menselijke zijn een stem geven.
-				Ze spelen daarom letterlijk en figuurlijk met de theatrale ruimte via hun moderne,
-				alternatieve popmuziek over de psyche van de mens.
-				Overweldigende harp, ijle tot ruige vioolstreken, slaggitaar en veelzijdige zang.
-				Als je er eenmaal in zit, wil je er niet meer uit.
-				Een optreden van Out Of Skin is ruimtelijk, ongrijpbaar en ontvouwt zich gedurende
-				de show van verstild naar expressief.
-				Hun muziek is genreloos en zweeft ergens tussen Radiohead, Tamino en Sigur Rós.
-				</p>
-				<p>
-				Out Of Skin werd dit jaar twee keer avondwinnaar tijdens de Grote Prijs Van Nederland en
-				stond in de finale in Paradiso.
-				Eind 2016 wonnen ze de SENA Grote Prijs Van Rotterdam.
-				Waar frontman Wouter Mol eerst alleen was, was deze winst het begin van Out Of Skin.
-				Hun debuut-ep volgde, live opgenomen in de theaterstudio van Rikke Korswagen en
-				voorzien met artwork van fotograaf Richard Beukelaar.
-				Vanaf augustus 2018 zullen een aantal nieuwe singles volgen,
-				opgenomen bij Simon Akkermans in het legendarische Kytopia.
-				</p>
-				<p>
-				Vindt je het leuk om op de hoogte te blijven? Volg de band op Spotify,
-				Soundcloud, YouTube, Instagram, Facebook en geef je mailadres op via
-				band@outofskin.com voor de nieuwsbrief!
-				</p>
-
-				<p>
-				<strong>
-					‘Kom uit je schulp en kijk eens naar binnen bij Out Of Skin'
-				</strong>
-				</p>
-				<p>
-				Wouter Mol: gitaar en zang
-					<br/>
-				Maartje Gilissen: harp en percussie
-					<br/>
-				Margot Kersing: viool
-				</p>
+				<MarkdownPreview text={this.props.bio}/>
 			</div>
 		);
 	}
