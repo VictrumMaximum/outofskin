7552db152c5b57ae0da7dff962ad0d61c814e6a4
Victor Ionescu <v.ionescu@student.tudelft.nl>
2018-05-31 03:10:04 +0200

    add confirm and error message for subscribing

--- src/client/site/Pages/Contact/index.tsx
+++ src/client/site/Pages/Contact/index.tsx
@@ -9,27 +9,34 @@ require("../../media/images/icons/phone.svg");
 require("../../media/images/icons/email.svg");
 
 interface ContactState {
-	email: string
+	email: string;
+	subscribed: boolean;
+	subscribeErrorMessage: string;
 }
 
 export default class Contact extends React.Component<{}, ContactState> {
 	constructor(props) {
 		super(props);
 		this.state = {
-			email: ""
+			email: "",
+			subscribed: false,
+			subscribeErrorMessage: ""
 		};
 		this.handleClick = this.handleClick.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleClick() {
+		this.setState({subscribeErrorMessage: ""});
 		axios.post(subscribersDataRoute, {email: this.state.email}).then((response: AxiosResponse) => {
 			const responseData = response.data;
 			if (responseData.error) {
 				console.log(JSON.stringify(responseData.error, null, 2));
+				this.setState({subscribeErrorMessage: JSON.stringify(responseData.error, null, 2)});
 			}
 			else {
 				console.log("success");
+				this.setState({subscribed: true});
 			}
 		});
 	}
@@ -41,6 +48,11 @@ export default class Contact extends React.Component<{}, ContactState> {
 	}
 
 	render() {
+		let subscribeButton = <div className={styles.boxLink} onClick={this.handleClick}>subscribe</div>;
+		if (this.state.subscribed) {
+			subscribeButton = <span style={{color: "green"}}>Bedankt!</span>;
+		}
+
 		return (
 			<div>
 				<table className={styles.information}>
@@ -71,9 +83,10 @@ export default class Contact extends React.Component<{}, ContactState> {
 				</table>
 				<br/>
 				Meld je aan voor de nieuwsbrief!<br/>
+				<span style={{color: "red"}}>{this.state.subscribeErrorMessage}</span><br/>
 				<input placeholder={"email"} value={this.state.email} onChange={this.handleChange}/>
 				<div className={styles.boxLinkWrapper}>
-					<div className={styles.boxLink} onClick={this.handleClick}>subscribe</div>
+					{subscribeButton}
 				</div>
 				<br/><br/>
 			</div>
