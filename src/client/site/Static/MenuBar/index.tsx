54ba7a6adee5ed45d7d38089b65f9b6eae8a40e5
Victor Ionescu <v.ionescu@student.tudelft.nl>
2018-06-01 12:50:21 +0200

    first attempt at mobile menu

--- src/client/site/Static/MenuBar/index.tsx
+++ src/client/site/Static/MenuBar/index.tsx
@@ -2,19 +2,45 @@ import * as React from "react";
 import Option from "./Option";
 import routes from "../../routes";
 const styles = require("./styles.less");
+import ReactSVG from 'react-svg';
+require("../../media/images/icons/menu.svg");
 
-export default class MenuBar extends React.Component<{}, {}> {
+interface MenuBarState {
+	toggleMenu: boolean;
+}
+
+export default class MenuBar extends React.Component<{}, MenuBarState> {
+	constructor(props) {
+		super(props);
+		this.state = {
+			toggleMenu: false
+		};
+	}
+
+	toggleDisplay() {
+		this.setState({
+			toggleMenu: !this.state.toggleMenu
+		});
+	}
 
     render() {
+		const menuDisplayStyle = "block";//this.state.toggleMenu ? "block" : "none";
         return (
 			<div className={styles.menuBar}>
-				{Object.keys(routes).map((path) => {
-					return <Option
-						key={path}
-						title={routes[path].title}
-						path={path}
-					/>
-				})}
+				<ReactSVG
+					path={"../images/menu.svg"}
+					svgClassName={styles.menuIcon}
+					onClick={this.toggleDisplay.bind(this)}
+				/>
+				<div className={styles.menuOptionsWrapper} style={{display: menuDisplayStyle}}>
+					{Object.keys(routes).map((path) => {
+						return <Option
+							key={path}
+							title={routes[path].title}
+							path={path}
+						/>
+					})}
+				</div>
 			</div>
         );
     }
