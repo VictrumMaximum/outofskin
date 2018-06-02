9b0396e5e61598a0fb4318e520319498b9b1207e
Victor Ionescu <victorpionescu@gmail.com>
2018-06-03 01:55:05 +0200

    properly close menu when clicking or touching outside of it

--- src/client/site/Static/MenuBar/Option.tsx
+++ src/client/site/Static/MenuBar/Option.tsx
@@ -6,6 +6,7 @@ const style = require("./styles.less");
 interface OptionProps {
     title: string;
     path: string;
+    hideMenu: () => void;
 }
 
 export default class Option extends React.Component<OptionProps, {}> {
@@ -15,7 +16,8 @@ export default class Option extends React.Component<OptionProps, {}> {
 			<div className={style.optionDiv}>
 				<h1>
 					<Link to={this.props.path}
-						className={style.optionLink}>
+						className={style.optionLink}
+						onClick={this.props.hideMenu}>
 						{this.props.title}
 						</Link>
 				</h1>
--- src/client/site/Static/MenuBar/index.tsx
+++ src/client/site/Static/MenuBar/index.tsx
@@ -6,38 +6,66 @@ import ReactSVG from 'react-svg';
 require("../../media/images/icons/menu.svg");
 
 interface MenuBarState {
-	toggleMenu: boolean;
+	showMenu: boolean;
 }
 
 export default class MenuBar extends React.Component<{}, MenuBarState> {
+	hideMenuOnBlur = (event) => {
+        const menuBar = document.getElementById(styles.menuBar);
+        let targetElement = event.target;
+		// check if selected element is part of menubar
+        do {
+            if (targetElement == menuBar || targetElement["id"] == "Layer_1") {
+            	// part of menubar, do nothing
+                return;
+            }
+            targetElement = targetElement["parentNode"];
+        } while (targetElement);
+        // outside menubar, set showMenu to false
+		// by calling toggleDisplay (also removes event listeners)
+        this.toggleDisplay();
+    };
+
 	constructor(props) {
 		super(props);
 		this.state = {
-			toggleMenu: false
+			showMenu: false
 		};
+		this.toggleDisplay = this.toggleDisplay.bind(this);
 	}
 
 	toggleDisplay() {
+		// add event listeners if opening the menu,
+		// remove event listeners when closing the menu
+		if (this.state.showMenu === true) {
+            document.removeEventListener("mousedown", this.hideMenuOnBlur);
+            document.removeEventListener("touchstart", this.hideMenuOnBlur);
+        } else {
+            document.addEventListener("mousedown", this.hideMenuOnBlur);
+            document.addEventListener("touchstart", this.hideMenuOnBlur);
+		}
 		this.setState({
-			toggleMenu: !this.state.toggleMenu
+			showMenu: !this.state.showMenu
 		});
 	}
 
     render() {
-		const menuDisplayStyle = this.state.toggleMenu ? "block" : "none";
+		const menuDisplayStyle = this.state.showMenu ? "block" : "none";
         return (
-			<div className={styles.menuBar}>
+			<div
+				id={styles.menuBar}>
 				<ReactSVG
 					path={"../images/menu.svg"}
-					svgClassName={styles.menuIcon}
-					onClick={this.toggleDisplay.bind(this)}
+					id={styles.menuIcon}
+					onClick={this.toggleDisplay}
 				/>
-				<div className={styles.menuOptionsWrapper} style={{display: menuDisplayStyle}}>
+				<div id={styles.menuOptionsWrapper} style={{display: menuDisplayStyle}}>
 					{Object.keys(routes).map((path) => {
 						return <Option
 							key={path}
 							title={routes[path].title}
 							path={path}
+							hideMenu={this.toggleDisplay}
 						/>
 					})}
 				</div>
