29cc28dc9d885532ffc5d8d63a522a96ddec49d0
Victor Ionescu <victorpionescu@gmail.com>
2018-07-09 02:26:52 +0200

    italize quotes

--- src/client/site/Pages/Press/Quote.tsx
+++ src/client/site/Pages/Press/Quote.tsx
@@ -15,7 +15,7 @@ export default class Quote extends React.Component<QuoteProps, {}> {
     render() {
         return (
 	        <div className={classNameSeparator(styles.quote, pageStyles.whitePlaneBackground)}>
-		        <div>{this.props.quote}</div>
+				<div><i>{this.props.quote}</i></div>
 				- <strong>{this.props.author}</strong>
 				<div className={styles.buttonWrapper}>
 					<Button text={"Bron"} action={() => {window.open(this.props.link)}} height={"0.3em"}/>
