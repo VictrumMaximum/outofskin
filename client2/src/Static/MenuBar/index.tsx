948ee55c2e490322a116531af5fd6140a0a641ac
Victor Ionescu <victorpionescu@gmail.com>
2019-07-08 15:11:47 +0200

    music and menubar styles

--- client2/src/Static/MenuBar/index.tsx
+++ client2/src/Static/MenuBar/index.tsx
@@ -69,7 +69,7 @@ export default class MenuBar extends React.Component<{}, MenuBarState> {
 	}
 
     render() {
-		const menuMaxHeight = this.state.showMenu ? "11em" : "0";
+		const menuMaxHeight = this.state.showMenu ? "13em" : "0";
 		const menuIconRotation = this.state.showMenu ? "90deg" : "0";
 		const pageIndicatorOpacity = this.state.showMenu ? 0 : 1;
         return (
@@ -81,16 +81,18 @@ export default class MenuBar extends React.Component<{}, MenuBarState> {
 				{/*	onClick={this.toggleDisplay}*/}
 				{/*	style={{transform: "rotate(" + menuIconRotation + ")"}}*/}
 				{/*/>*/}
-				<MenuIcon
-					id={styles.menuIcon}
-					onClick={this.toggleDisplay}
-					style={{transform: "rotate(" + menuIconRotation + ")"}}
-				/>
-				<div id={styles.mobilePageIndicator}
-					 style={{opacity: pageIndicatorOpacity}}
-					 onClick={this.toggleDisplay}>
-					{this.state.selected}
-					 </div>
+				<div id={styles.menuButton}>
+					<MenuIcon
+						id={styles.menuIcon}
+						onClick={this.toggleDisplay}
+						style={{transform: "rotate(" + menuIconRotation + ")"}}
+					/>
+					<div id={styles.mobilePageIndicator}
+						 style={{opacity: pageIndicatorOpacity}}
+						 onClick={this.toggleDisplay}>
+						{this.state.selected}
+					</div>
+				</div>
 				<div id={styles.menuOptionsWrapper}
 					 style={{maxHeight: menuMaxHeight}}>
 					{Object.keys(routes).map((path) => {
