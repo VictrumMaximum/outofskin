55478c1319a447c741cb5a94e163fa7b3eba37e7
Victor Ionescu <v.ionescu@student.tudelft.nl>
2019-09-05 00:11:20 +0200

    fix bug where any path redirects to homepage

--- client2/src/MainContainer.tsx
+++ client2/src/MainContainer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Route, BrowserRouter, Redirect} from "react-router-dom";
+import {Route, BrowserRouter, Redirect, Switch} from "react-router-dom";
 import {routes, homepage} from "./routes";
 import MenuBar from "./Static/MenuBar/index";
 import SocialMedia from "./Static/SocialMedia/index";
@@ -8,6 +8,8 @@ import styles from "./styles.module.scss";
 
 export default class MainContainer extends React.Component<{}, {}> {
 	render() {
+		// Switch is needed to let Redirect work properly:
+		// https://github.com/ReactTraining/react-router/issues/4837
 		return (
 			<BrowserRouter>
 				{/*router may only have 1 child element*/}
@@ -16,7 +18,9 @@ export default class MainContainer extends React.Component<{}, {}> {
 					<div id={styles.contentSidebar}>
 						<SocialMedia/>
 						<div id={styles.content} className={"row grow"}>
-							<Redirect from={"/"} to={homepage} />
+							<Switch>
+								<Redirect exact path={"/"} to={homepage} />
+							</Switch>
 							{Object.keys(routes).map(path =>
 								(<Route
 									key={path}
