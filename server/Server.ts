77a69944d937e2767a21a9f8105d4d8061bdcf2d
Victor Ionescu <v.ionescu@student.tudelft.nl>
2020-03-18 20:24:40 +0100

    this should be it

--- server/Server.ts
+++ server/Server.ts
@@ -1,4 +1,7 @@
 import * as express from "express";
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
 import * as dataRoutes from "./DataRouters/dataRoutes";
 import {resolve} from "path";
 import "./env";
@@ -10,7 +13,8 @@ import pressRouter from "./DataRouters/PressRouter";
 import Logger from "./Logs/Logger";
 
 const app = express();
-const port = process.env.PORT;
+const portHttp = process.env.PORT_HTTP;
+const portHttps = process.env.PORT_HTTPS;
 
 // to support JSON-encoded bodies
 app.use(express.json());
@@ -22,14 +26,44 @@ app.use(dataRoutes.subscribersDataRoute, subscribersRouter);
 app.use(dataRoutes.musicDataRoute, musicRouter);
 app.use(dataRoutes.pressDataRoute, pressRouter);
 
-// for ssl certificate
-app.use('/.well-known', express.static(resolve(__dirname + '/../.well-known/')));
-
 app.use("/menu", express.static(__dirname + "/../../client2/menuBuild/"));
-// for static files except index.html
-app.use(express.static(resolve(__dirname + '/../../client2/build/')));
-// any other route should return index.html (specific route is handled by react router)
-app.get('/*', (req, res): void => {res.sendFile(resolve(__dirname + '/../../client2/build/index.html'))});
 
-app.listen(port);
-Logger.info("Server started on port " + port);
+
+if (process.env.NODE_ENV === 'production') {
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/outofskin.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/outofskin.com/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/outofskin.com/chain.pem', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+
+    // for ssl certificate
+    app.use('/.well-known', express.static(resolve(__dirname + '/../.well-known/')));
+    // for static files except index.html
+    app.use(express.static(resolve(__dirname + '/../../client2/build/')));
+    // any other route should return index.html (specific route is handled by react router)
+    app.get('/*', (req, res): void => {res.sendFile(resolve(__dirname + '/../../client2/build/index.html'))});
+
+    http.createServer((req, res): void => {
+        // redirects to secure site
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    }).listen(portHttps, (): void => {
+        Logger.info('HTTP Server running on port ' + portHttp);
+    });
+
+    https.createServer(credentials, app)
+        .listen(portHttps, (): void => {
+            Logger.info('HTTPS Server running on port ' + portHttps);
+        });
+} else {
+    http.createServer(app).listen(portHttp, (): void => {
+        Logger.info('HTTP Server running on port ' + portHttp);
+    });
+}
+
+// app.listen(portHttp);
+// Logger.info("Server started on port " + port);
